401b7bf3fcb83396e13e1a347cb0b2103925aa10
Seiya Nuta <nuta@seiya.me>
2019-07-19 12:12:22 +0900

    wip

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -1224,6 +1224,9 @@ static void disassembleObject(const Target *TheTarget, const ObjectFile *Obj,
     SmallString<40> Comments;
     raw_svector_ostream CommentStream(Comments);
 
+    SmallString<128> InstString;
+    raw_svector_ostream InstStringStream(InstString);
+
     ArrayRef<uint8_t> Bytes = arrayRefFromStringRef(
         unwrapOrError(Section.getContents(), Obj->getFileName()));
 
@@ -1383,9 +1386,10 @@ static void disassembleObject(const Target *TheTarget, const ObjectFile *Obj,
 
         PIP.printInst(
             *IP, Disassembled ? &Inst : nullptr, Bytes.slice(Index, Size),
-            {SectionAddr + Index + VMAAdjustment, Section.getIndex()}, outs(),
-            "", *STI, &SP, &Rels);
-        outs() << CommentStream.str();
+            {SectionAddr + Index + VMAAdjustment, Section.getIndex()},
+            InstStringStream, "", *STI, &SP, &Rels);
+        outs() << InstStringStream.str() << CommentStream.str();
+        InstString.clear();
         Comments.clear();
 
         // Try to resolve the target of a call, tail call, etc. to a specific
